1eca41ad46bcde25eb7d41a28141f4e9661fb408
675861708@qq.com <qubaoming@didichuxing.com>
2016-11-23 22:43:23 +0800

    add layout

---
+++ demo_layout_animation/android/app/src/main/java/com/demo_layout_animation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.demo_layout_animation;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "demo_layout_animation";
+    }
+}
